d461621dfd9b095b2554f718bd09764920f1b629
mura <79096051+nicoluvas@users.noreply.github.com>
2023-07-12 18:32:00 -0300

    peace
    pice

---
+++ beta.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const ytdl = require("ytdl-core");
+const fs = require('fs');
+const app = express();
+
+app.get('/download/video', async function (req, res) {
+    const videoUrl = req.query.url;
+    const videoInfo = await ytdl.getInfo(videoUrl);
+    const videoTitle = videoInfo.videoDetails.title;
+    const fileName = `${videoTitle}.mp4`;
+  
+    ytdl(videoUrl, { filter: 'audioandvideo' })
+      .pipe(fs.createWriteStream(fileName));
+  
+    res.attachment(fileName);
+    res.send('Download do vídeo iniciado!');
+  });
+  
+  app.get('/download/audio', async function (req, res) {
+    const videoUrl = req.query.url;
+    const videoInfo = await ytdl.getInfo(videoUrl);
+    const videoTitle = videoInfo.videoDetails.title;
+    const fileName = `${videoTitle}.mp3`;
+  
+    ytdl(videoUrl, { filter: 'audioonly' })
+      .pipe(fs.createWriteStream(fileName));
+  
+    res.attachment(fileName);
+    res.send('Download do áudio iniciado!');
+  });
+
+app.listen(3000);
+
+
+//zé da manga:
+//https://www.youtube.com/watch?v=2F1zi6MDy6k
+
